01f9448cebacdc57f64a534b843405a427617941
diemonch <ravishankard_2k9@yahoo.com>
2025-02-14 18:52:21 +0530

    Added workernode for EKS

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,7 +1,14 @@
 resource "aws_ecr_repository" "react_app_repo" {
-  name                 = "react-app"
+  name         = "react-app"
+  force_delete = true  # Optional: Allows Terraform to delete the repo if needed
+
   image_scanning_configuration {
-    scan_on_push = true
+    scan_on_push = true  # ✅ Automatically scans images for vulnerabilities on push
+  }
+
+  tags = {
+    Name        = "react-app-ecr"
+    Environment = "production"
   }
 }
 
