6de41581cc4718d52a1f7a739eb03b1761ce434c
diemonch <ravishankard_2k9@yahoo.com>
2025-02-14 18:40:14 +0530

    Added workernode for EKS

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -19,3 +19,25 @@ resource "aws_iam_role_policy_attachment" "eks_role_policy" {
   role       = aws_iam_role.eks_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
+
+resource "aws_iam_role" "worker_node_role" {
+  name = "eks-worker-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "worker_node_policy" {
+  role       = aws_iam_role.worker_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
