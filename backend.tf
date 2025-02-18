b49e9f5300a267b4363d19406cd0ad7bd1f35ad2
diemonch <ravishankard_2k9@yahoo.com>
2025-02-18 21:28:27 +0530

    Removed .terraform & state files from Git tracking

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-terraform-state-buckets1"  # 🔹 Replace with your actual S3 bucket name
-    key            = "eks-cluster/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock"
-    encrypt        = true
-  }
-}
